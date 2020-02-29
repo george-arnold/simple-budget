4d7baa0a2e82b4135dc6213feebb0e09fbc4363a
George Arnold <george.arnold.88@gmail.com>
2020-02-29 13:45:32 -0500

    cleanup

--- src/SpendingTracker/SpendingTracker.js
+++ src/SpendingTracker/SpendingTracker.js
@@ -8,7 +8,7 @@ class SpendingTracker extends Component {
     return (
       <main className="SpendingTracker">
         <h2>Your Spending</h2>
-        <p>Total Amount Spent></p>
+        <p>Total Amount Spent</p>
         <h3>{this.context.totalCost}</h3>
       </main>
     );
