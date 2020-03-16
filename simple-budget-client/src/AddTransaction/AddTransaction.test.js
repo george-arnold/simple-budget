a6620ecdbb3bf9cdebca8db8a55a0d5277623cc7
George Arnold <george.arnold.88@gmail.com>
2020-03-15 20:10:37 -0400

    fixing npm

--- simple-budget-client/src/AddTransaction/AddTransaction.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import AddTransaction from './AddTransaction';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<AddTransaction/>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
